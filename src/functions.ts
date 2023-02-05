c0ef5070240c63ca875c0a93a22349a9303f60fc
Isadora <dorisperdigao@gmail.com>
2023-02-04 22:27:27 -0300

    feature: creates route to list movies

--- src/functions.ts
+++ src/functions.ts
@@ -1,5 +1,5 @@
 import { request, Request, Response } from "express";
-import format from "pg-format";
+import format, { string } from "pg-format";
 import { client } from "./database";
 import { iMovie, MovieCreate, MovieResult } from "./interfaces";
 
@@ -34,4 +34,73 @@ const createMovie = async (request: Request, response: Response) : Promise<Respo
     return response.status(201).json(queryResult.rows)
 }
 
-export { createMovie }
+const getPreviousPageURL = (request: Request) : string => {
+
+    let page = request.query.page
+    let previousPageURL = "http://localhost:3000/movies?"
+    const sort = request.query.sort
+    const order = request.query.order
+    const perPage = parseInt(request.query.perPage as string)
+
+    if(parseInt(page as string) > 1) {
+        previousPageURL += `page=${parseInt(page as string) - 1}&perPage=${perPage}`
+    }
+
+    previousPageURL += `&order=${order}`
+
+    previousPageURL += `&sort=${sort}`
+   
+
+    return previousPageURL
+    
+}
+
+const getNextPageURL = (request: Request) : string => {
+    let page = request.query.page
+    let nextPageURL = "http://localhost:3000/movies?"
+    const sort = request.query.sort
+    const order = request.query.order
+    const perPage = parseInt(request.query.perPage as string)
+
+    
+    nextPageURL += `page=${parseInt(page as string) + 1}&perPage=${perPage}`
+
+    nextPageURL += `&order=${order}`
+
+    nextPageURL += `&sort=${sort}`
+   
+
+    return nextPageURL
+}
+
+const listMovies = async (request: Request, response: Response) : Promise<Response> => {
+    const queryParams = request.query
+    const limit = parseInt(queryParams.perPage as string)
+    const offset = (parseInt(queryParams.page as string) - 1) * limit
+    const sort = queryParams.sort
+    const order = queryParams.order
+    let orderClause = ""
+    let page = queryParams.page
+
+    if(order) {
+        orderClause = `ORDER BY ${order} ${sort}`
+    }
+
+    const query = `
+        SELECT * FROM movies ${orderClause} LIMIT ${limit} OFFSET ${offset};
+    `
+    
+    const queryResult = await client.query(query)
+    
+    let previousPage: string | null = parseInt(page as string) > 1 ? getPreviousPageURL(request) : null
+    let nextPage: string = getNextPageURL(request)
+
+    return response.status(200).json({
+        previousPage,
+        nextPage,
+        count: queryResult.rowCount,
+        data: queryResult.rows
+    })
+}
+
+export { createMovie, listMovies }
--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express"
+import { Request, Response, NextFunction, request } from "express"
 import { client } from "./database"
 
 const validateNewMovie = async (request: Request, response: Response, next: NextFunction) => {
@@ -18,4 +18,59 @@ const validateNewMovie = async (request: Request, response: Response, next: Next
     return next()
 }
 
-export { validateNewMovie }
+const parsePageParam = (request: Request, response: Response, next: NextFunction) => {
+    const urlQuery = request.query
+    let page = "1"
+
+    if(urlQuery.page && parseInt(urlQuery.page as string) && parseInt(urlQuery.page as string) > 0 ) {
+        page = urlQuery.page as string
+    }
+
+    request.query.page = page
+
+    return next()
+}
+
+const parsePerPageParam = (request: Request, response: Response, next: NextFunction) => {
+    const urlQuery = request.query
+    let perPage = "5"
+    const requestedPerPage = parseInt(urlQuery.perPage as string)
+
+    if(urlQuery.perPage && requestedPerPage && requestedPerPage > 0 && requestedPerPage < 5) {
+        perPage = urlQuery.perPage as string
+    }
+
+    request.query.perPage = perPage
+
+    return next()
+}
+
+const parseOrderParam = (request: Request, response: Response, next: NextFunction) => {
+    const validQuerys: string[] = ["price", "duration"]
+    const requestedOrder = request.query.order as string  
+    let order = "" 
+    
+    if(validQuerys.includes(requestedOrder)) {
+        order = requestedOrder
+    }
+    
+    request.query.order = order
+
+    return next()
+}
+
+const parseSortParam = (request: Request, response: Response, next: NextFunction) => {
+    const validQuerys: string[] = ["asc", "desc"]
+    const requestedSort = request.query.sort as string  
+    let sort = "" 
+    
+    if(validQuerys.includes(requestedSort)) {
+        sort = requestedSort
+    }
+    
+    request.query.sort = sort
+
+    return next()
+}
+
+export { validateNewMovie, parsePageParam, parsePerPageParam, parseOrderParam, parseSortParam }
